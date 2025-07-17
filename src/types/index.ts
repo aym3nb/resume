4dc8d3ad189d449af8d125042316e79c3b4792ce
aym3nb <aym3nb@gmail.com>
2025-07-17 14:00:49 +0200

    🏷️ Add types

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export interface Contact {
+  email: string
+  mobile: string
+  github: string
+}
+
+export interface Experience {
+  company: string
+  location: string
+  role: string
+  period: string
+  highlights: string[]
+}
+
+export interface Education {
+  degree: string;
+  institution: string;
+  period: string;
+  location: string;
+}
